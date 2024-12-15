d983254a27b38b2b987724640f2f8976dda150bb
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2024-12-16 03:13:05 +0600

    js hovers and clicks

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showMenu() {
 };
 
 function hideMenu() {
-    menu[0].style.left = '-297px';
+    menu[0].style.left = '-290px';
     overlay.style.display = 'none';
 };
 
@@ -20,4 +20,34 @@ document.addEventListener('click', function(event) {
     if (!search[0].contains(event.target)) {
         search[0].setAttribute('id', 'mobileSearch'); 
     }
-  });
+});
+
+//submenu hover javascript
+
+const submenu = document.getElementsByClassName('submenu');
+const cat = document.getElementsByClassName('category')[0].children;
+
+for (let i = 1; i < cat.length; i++) {
+    cat[i].addEventListener('mouseover', function () {
+        if (window.innerWidth > 1280) {
+            submenu[i - 1].style.display = 'block';
+        }
+    })
+
+    cat[i].addEventListener('click', function () {
+        if (window.innerWidth <= 1280) {
+            if (submenu[i - 1].style.display === 'block') {
+                submenu[i - 1].style.display = 'none';
+            }
+            else {
+                submenu[i - 1].style.display = 'block';
+            }
+        }
+    })
+
+    cat[i].addEventListener('mouseout', function () {
+        if (window.innerWidth > 1280) {
+            submenu[i - 1].style.display = 'none';
+        }
+    })
+}
