26b38e6dd5b3a643fcb7f14a4d4ea65103cb4bd8
plabon6 <ahsanulazim6@gmail.com>
2024-12-17 19:26:46 +0600

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,100 @@
-const menu = document.getElementsByClassName("bottomHeader");
+fetch('Data/product.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        renderCategories(data.categories);
+      })
+      .catch(error => {
+        console.error('Error fetching JSON:', error);
+      });
+
+
+    // Function to render categories in the DOM
+    function renderCategories(categories) {
+      const container = document.getElementById("category");
+      const mobContainer = document.getElementById('mobCategory');
+
+      //Off Canvas Loop Category
+    categories.forEach(mobCat => {
+        const mobCatLi = document.createElement("li");
+        mobCatLi.className = "mainCat";
+        mobCatLi.innerHTML = `<div>${mobCat.name}<i class="fa-solid fa-angle-down downArrow"></i></div>`;
+
+        const subcategoryList = document.createElement("ul");
+        subcategoryList.className = "mobSubCat";
+
+        mobCat.subcategories.forEach(subcategory => {
+            const mobSubCatItem = document.createElement("li");
+            mobSubCatItem.innerHTML = `<a href="#">${subcategory}</a>`;
+            subcategoryList.appendChild(mobSubCatItem);
+          });
+          mobCatLi.appendChild(subcategoryList);
+          mobContainer.appendChild(mobCatLi);
+
+
+    });
+
+      // Loop through each category
+      categories.forEach(category => {
+        const categoryLi = document.createElement("li");
+        categoryLi.className = "mainCat";
+        categoryLi.textContent = category.name;
+
+    // Create the subcategories list
+        const subcategoryList = document.createElement("ul");
+        subcategoryList.className = "subcategory";
+
+        category.subcategories.forEach(subcategory => {
+          const subcategoryItem = document.createElement("li");
+          subcategoryItem.textContent = subcategory;
+          subcategoryList.appendChild(subcategoryItem);
+        });
+
+        categoryLi.appendChild(subcategoryList);
+        container.appendChild(categoryLi);
+      });
+
+    const mobSubCat = document.getElementsByClassName('mobSubCat')
+    const downArrow = document.getElementsByClassName('downArrow')
+
+for (let i = 0; i < mobSubCat.length; i++) {
+    downArrow[i].addEventListener('click', function () {
+        if (mobSubCat[i].classList.contains('showSub')) {
+            mobSubCat[i].classList.remove('showSub');  
+            downArrow[i].style.transform = 'none';
+        }
+        else{
+            mobSubCat[i].classList.add('showSub');
+            downArrow[i].style.transform = 'rotate(-180deg)';
+        }
+    })
+    
+};
+    }
+
+const offCanvas = document.getElementById("offCanvas")
 const overlay = document.getElementById('overlay');
-const search = document.getElementsByClassName('search')
-const searchInput = document.getElementById('searchInput')
+
 function showMenu() {
-    menu[0].style.left = 0;
+    offCanvas.style.left = 0;
     overlay.style.display = 'block';
 };
 
 function hideMenu() {
-    menu[0].style.left = '-290px';
+    offCanvas.style.left = '-280px';
     overlay.style.display = 'none';
 };
 
-search[0].addEventListener('click', function () {   
-    search[0].removeAttribute('id','mobileSearch');
-});
+const mobSubCat = document.getElementsByClassName('mobSubCat')
+const downArrow = document.getElementsByClassName('downArrow')
 
-document.addEventListener('click', function(event) {
-    if (!search[0].contains(event.target)) {
-        search[0].setAttribute('id', 'mobileSearch'); 
-    }
-});
-
-//submenu hover javascript
-
-const submenu = document.getElementsByClassName('submenu');
-const cat = document.getElementsByClassName('category')[0].children;
-const mainCat = document.getElementsByClassName('mainCategory');
-
-for (let i = 1; i < cat.length; i++) {
-    cat[i].addEventListener('mouseover', function () {
-        if (window.innerWidth > 1280) {
-            submenu[i - 1].style.display = 'block';
-        }
+for (let i = 0; i < mobSubCat.length; i++) {
+    downArrow[i].addEventListener('click', function () {
+        mobSubCat[i].classList.add('showSub');  
     })
-
-    mainCat[i-1].addEventListener('click', function () {
-        if (window.innerWidth <= 1280) {
-            if (submenu[i - 1].style.display === 'block') {
-                submenu[i - 1].style.display = 'none';
-            }
-            else {
-                submenu[i - 1].style.display = 'block';
-            }
-        }
-    })
-
-    cat[i].addEventListener('mouseout', function () {
-        if (window.innerWidth > 1280) {
-            submenu[i - 1].style.display = 'none';
-        }
-    })
-}
+    
+};
